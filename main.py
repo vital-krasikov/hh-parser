907a7270b203d36e175c1e968cf079f9e5e3a28f
Krasikov.VA <vital.krasikov@gmail.com>
2022-07-22 20:41:08 +0300

    22.07.2022 added database support

--- main.py
+++ main.py
@@ -2,16 +2,18 @@ from client import Parser
 from wordcloud import WordCloud
 import matplotlib.pyplot as plt
 
-p = Parser()
+p = Parser(1000, 0.1)
 # сначала пробовал делать дополнительные параметры для этого метода, чтобы указать, например, город,
 # но потом оказалось, что поиск hh отлично работает и с просто добавлением города в поисковую строку,
 # а через их справочники куда сложнее регион выбирать
-salary = p.get_vacancies('Программист Красноярск')
+salary = p.get_vacancies('Разработчик Python')
 print('Проанализировано вакансий: {}, средняя зарплата от {:.2f} до {:.2f}'.format(salary['found'],
                                                                                    salary['min'], salary['max']))
-skills = p.get_skills()
+skills = p.update_skills()
 wordcloud = WordCloud(width=900, height=500, max_words=1628, relative_scaling=1, normalize_plurals=False).generate_from_frequencies(skills)
 
 plt.imshow(wordcloud, interpolation='bilinear')
 plt.axis("off")
-plt.show()
+#plt.show()
+
+plt.savefig('./static/img/image.png', transparent=True)
--- web-main.py
+++ web-main.py
@@ -1,8 +1,20 @@
 from flask import Flask, render_template, request
 from client import Parser
 
+MAX_VACANCIES = 100
+MIN_SKILLS_FREQ = 0.1
+
+
+class Output:
+
+    def __init__(self):
+        self.salary = ''
+        self.result_list = []
+
+
 app = Flask(__name__)
-p = Parser(100, 0.1)
+p = Parser(MAX_VACANCIES, MIN_SKILLS_FREQ)
+output = Output()
 
 
 @app.route('/')
@@ -23,34 +35,26 @@ def form_post():
             if 0 <= new_value <= 2000:
                 p.max_vacancies = new_value
         except ValueError:
-            p.max_vacancies = 100
+            p.max_vacancies = MAX_VACANCIES
         try:
             new_value = float(request.form['min_skills_freq'])
             if 0 <= new_value <= 1:
                 p.min_skills_freq = new_value
         except ValueError:
-            p.min_skills_freq = 0.1
+            p.min_skills_freq = MIN_SKILLS_FREQ
 
         return render_template("form.html", max_vacancies=p.max_vacancies, min_skills_freq=p.min_skills_freq)
     else:
         text = request.form['query']
         p.query = text
 
-        salary = p.get_vacancies(text)
-        skills = p.get_skills()
+        output.salary = p.get_vacancies(text)
+        p.update_skills()
 
-        sorted_skills = sorted(skills.items(), key=lambda x: x[1], reverse=True)
+        output.result_list = ['Проанализировано вакансий: {},'.format(output.salary['found']),
+                       'средняя зарплата от {:.2f} до {:.2f}'.format(output.salary['min'], output.salary['max'])]
 
-        result_list = ['Проанализировано вакансий: {},'.format(salary['found']),
-                       'средняя зарплата от {:.2f} до {:.2f}'.format(salary['min'], salary['max'])]
-
-        for skill in sorted_skills:
-            if skill[1] >= p.min_skills_freq * salary['found']:
-                result_list.append(skill[0] + ': ' + str(skill[1]))
-            else:
-                break
-
-        return render_template("results.html", result=result_list)
+        return render_template("results.html", result=output.result_list, img_url='/static/img/'+text.replace(' ', '_')+'.png')
 
 
 @app.route('/contacts/')
@@ -60,7 +64,15 @@ def contacts():
 
 @app.route('/results/')
 def results():
-    return render_template("results.html")
+    sorted_skills = sorted(p.skills.items(), key=lambda x: x[1], reverse=True)
+
+    for skill in sorted_skills:
+        if skill[1] >= p.min_skills_freq * output.salary['found']:
+            output.result_list.append(skill[0] + ': ' + str(skill[1]))
+        else:
+            break
+
+    return render_template("results_full.html", result=output.result_list, img_url='/static/img/'+p.query.replace(' ', '_')+'.png')
 
 
 if __name__ == "__main__":
